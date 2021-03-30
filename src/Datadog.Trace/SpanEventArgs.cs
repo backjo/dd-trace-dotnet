f1dca8cb82af4f22ffcfcce0eab6947c360f0bb0
Tony Redondo <tony.redondo@datadoghq.com>
2021-03-31 00:37:16 +0200

    Change the SpanEventArgs to struct type.

--- src/Datadog.Trace/SpanEventArgs.cs
+++ src/Datadog.Trace/SpanEventArgs.cs
@@ -5,15 +5,15 @@ namespace Datadog.Trace
     /// <summary>
     /// EventArgs for a Span
     /// </summary>
-    internal class SpanEventArgs : EventArgs
+    internal readonly struct SpanEventArgs
     {
+        internal readonly Span Span;
+
         /// <summary>
-        /// Initializes a new instance of the <see cref="SpanEventArgs"/> class.
+        /// Initializes a new instance of the <see cref="SpanEventArgs"/> struct.
         /// Creates a new <see cref="SpanEventArgs"/> using <paramref name="span"/>
         /// </summary>
         /// <param name="span">The <see cref="Span"/> used to initialize the <see cref="SpanEventArgs"/> object.</param>
         public SpanEventArgs(Span span) => Span = span;
-
-        internal Span Span { get; }
     }
 }
